638e6949347ec84a63e649f023b57ca05a036d3d
Jens Kraemer <jk@jkraemer.net>
2018-01-25 15:46:25 +0800

    change necessary for decidim 0.8 (context)

--- lib/decidim/members/participatory_space.rb
+++ lib/decidim/members/participatory_space.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Decidim.register_participatory_space(:members) do |participatory_space|
-  participatory_space.engine = Decidim::Members::Engine
-  participatory_space.admin_engine = Decidim::Members::AdminEngine
+  participatory_space.context(:public) do |ctx|
+    ctx.engine = Decidim::Members::Engine
+  end
+  participatory_space.context(:admin) do |ctx|
+    ctx.engine = Decidim::Members::AdminEngine
+  end
 end
